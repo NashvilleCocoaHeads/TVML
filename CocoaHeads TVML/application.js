cc0813b617240d8ad948038f667c4debc1b1c032
Michael Hulet <michael@hulet.tech>
2019-08-28 20:37:27 -0500

    Built a bare random quotes app at the meetup

--- CocoaHeads TVML/application.js
+++ CocoaHeads TVML/application.js
@@ -8,3 +8,45 @@
 //  Copyright © 2019 Nashville CocoaHeads. All rights reserved.
 //
 
+let baseURL;
+let quotes;
+
+App.onLaunch = async (options) => {
+    baseURL = options.BASEURL;
+    const view = await load("Views/Quote.xml");
+    const loader = loadingTemplate();
+    quotes = JSON.parse((await load("Data/Quotes.json")).response);
+
+    navigationDocument.replaceDocument(view.responseXML, loader);
+
+    loadQuote();
+
+    getActiveDocument().getElementById("next").addEventListener("select", loadQuote);
+};
+
+function loadQuote() {
+    const document = getActiveDocument();
+    const index = Math.floor(Math.random() * quotes.length);
+    document.getElementById("quote").innerHTML = quotes[index].title;
+    document.getElementById("attribution").innerHTML = "- " + quotes[index].attribution;
+    console.log(document.getElementById("image"));
+}
+
+async function load(resource) {
+    return new Promise((resolve, reject) => {
+                       const request = new XMLHttpRequest();
+                       request.open("GET", baseURL + resource);
+                       request.send();
+                       request.onload = () => {
+                       return resolve(request);
+                       };
+                       });
+}
+
+function loadingTemplate() {
+    var template = '<document><loadingTemplate><activityIndicator><text>Loading</text></activityIndicator></loadingTemplate></document>';
+    var templateParser = new DOMParser();
+    var parsedTemplate = templateParser.parseFromString(template, "application/xml");
+    navigationDocument.pushDocument(parsedTemplate);
+    return parsedTemplate;
+}
